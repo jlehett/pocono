e0de1bb3eee4ddfa3184e6fcee0d01c61962ec64
Tanner Lehett <john.cfplus@gmail.com>
2024-07-06 14:23:20 -0700

    Finish working on the Cond component.

--- packages/func-comp/src/components/Cond/index.test.tsx
+++ packages/func-comp/src/components/Cond/index.test.tsx
@@ -56,20 +56,25 @@ describe('Cond', () => {
     describe('when the Cond.If expression evaluates to false', () => {
         describe('and there are no Cond.ElseIf components', () => {
             describe('and there is a Cond.Else component', () => {
-                it('should render the Cond.Else children', () => {
-                    const { queryByText } = render(
-                        <Cond>
-                            <Cond.If expr={false}>
-                                <div>If True</div>
-                            </Cond.If>
-                            <Cond.Else>
-                                <div>Else</div>
-                            </Cond.Else>
-                        </Cond>,
-                    );
+                const getScenario = () => render(
+                    <Cond>
+                        <Cond.If expr={false}>
+                            <div>If True</div>
+                        </Cond.If>
+                        <Cond.Else>
+                            <div>Else</div>
+                        </Cond.Else>
+                    </Cond>
+                );
 
-                    expect(queryByText('If True')).not.toBeInTheDocument();
+                it('should render the Cond.Else children', () => {
+                    const { queryByText } = getScenario();
                     expect(queryByText('Else')).toBeInTheDocument();
+                });
+
+                it('should not render any other children', () => {
+                    const { queryByText } = getScenario();
+                    expect(queryByText('If True')).not.toBeInTheDocument();
                 });
             });
 
@@ -89,25 +94,136 @@ describe('Cond', () => {
         });
 
         describe('and there are Cond.ElseIf components', () => {
-            it('should render the Cond.ElseIf children of the first Cond.ElseIf whose expression evaluates to true', () => {
-                const { queryByText } = render(
-                    <Cond>
-                        <Cond.If expr={false}>
-                            <div>If True</div>
-                        </Cond.If>
-                        <Cond.ElseIf expr={false}>
-                            <div>ElseIf False</div>
-                        </Cond.ElseIf>
-                        <Cond.ElseIf expr={true}>
-                            <div>ElseIf True</div>
-                        </Cond.ElseIf>
-                    </Cond>,
-                );
+            const getScenario = () => render(
+                <Cond>
+                    <Cond.If expr={false}>
+                        <div>If True</div>
+                    </Cond.If>
+                    <Cond.ElseIf expr={false}>
+                        <div>ElseIf False</div>
+                    </Cond.ElseIf>
+                    <Cond.ElseIf expr={true}>
+                        <div>ElseIf True</div>
+                    </Cond.ElseIf>
+                </Cond>
+            );
 
-                expect(queryByText('If True')).not.toBeInTheDocument();
-                expect(queryByText('ElseIf False')).not.toBeInTheDocument();
+            it('should render the Cond.ElseIf children of the first Cond.ElseIf whose expression evaluates to true', () => {
+                const { queryByText } = getScenario();
                 expect(queryByText('ElseIf True')).toBeInTheDocument();
             });
+
+            it('should not render any other children', () => {
+                const { queryByText } = getScenario();
+                expect(queryByText('If True')).not.toBeInTheDocument();
+                expect(queryByText('ElseIf False')).not.toBeInTheDocument();
+            });
+        });
+    });
+
+    describe('when there is no Cond.If child', () => {
+        const getScenario = () => render(
+            <Cond>
+                <div>Test</div>
+            </Cond>
+        )
+
+        it('should throw an error', () => {
+            expect(getScenario).toThrow();
+        });
+    });
+
+    describe('when multiple Cond.If children exist', () => {
+        const getScenario = () => render(
+            <Cond>
+                <Cond.If expr={true}>
+                    If True 1
+                </Cond.If>
+                <Cond.If expr={true}>
+                    If True 2
+                </Cond.If>
+            </Cond>
+        );
+
+        it('should throw an error', () => {
+            expect(getScenario).toThrow();
+        });
+    });
+
+    describe('when the Cond.If child is not the first child', () => {
+        const getScenario = () => render(
+            <Cond>
+                <Cond.ElseIf expr={true}>
+                    Else If True
+                </Cond.ElseIf>
+                <Cond.If expr={true}>
+                    If True
+                </Cond.If>
+            </Cond>
+        );
+
+        it('should throw an error', () => {
+            expect(getScenario).toThrow();
+        });
+    });
+
+    describe('when multiple Cond.Else children exist', () => {
+        const getScenario = () => render(
+            <Cond>
+                <Cond.If expr={true}>
+                    If True
+                </Cond.If>
+                <Cond.Else>
+                    Else 1
+                </Cond.Else>
+                <Cond.Else>
+                    Else 2
+                </Cond.Else>
+            </Cond>
+        );
+
+        it('should throw an error', () => {
+            expect(getScenario).toThrow();
+        });
+    });
+
+    describe('when a Cond.Else child exists but is not the last child', () => {
+        const getScenario = () => render(
+            <Cond>
+                <Cond.If expr={true}>
+                    If True
+                </Cond.If>
+                <Cond.Else>
+                    Else
+                </Cond.Else>
+                <Cond.ElseIf expr={true}>
+                    Else If True
+                </Cond.ElseIf>
+            </Cond>
+        );
+
+        it('should throw an error', () => {
+            expect(getScenario).toThrow();
+        });
+    });
+
+    describe('when a child exists that isn\'t a valid Cond subcomponent', () => {
+        const getScenario = () => render(
+            <Cond>
+                <Cond.If expr={true}>
+                    If True
+                </Cond.If>
+                <div>
+                    Invalid Child
+                </div>
+                <Cond.Else>
+                    Else True
+                </Cond.Else>
+            </Cond>
+        );
+
+        it('should throw an error', () => {
+            expect(getScenario).toThrow();
         });
     });
 });
