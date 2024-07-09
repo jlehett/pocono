f50064e1ba806b0dfdb810b7b60a8f78e703f65a
Tanner Lehett <john.cfplus@gmail.com>
2024-07-09 00:08:07 -0700

    v0.2.0

--- packages/func-comp/CHANGELOG.md
+++ packages/func-comp/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @pocono/func-comp
 
+## 0.2.0
+
+### Minor Changes
+
+-   Create `If` component.
+
 ## 0.1.0
 
 ### Minor Changes
--- packages/func-comp/src/index.ts
+++ packages/func-comp/src/index.ts
@@ -1,2 +1,3 @@
-export { default as Switch } from './components/Switch';
 export { default as Cond } from './components/Cond';
+export { default as If } from './components/If';
+export { default as Switch } from './components/Switch';
