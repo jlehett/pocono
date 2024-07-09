0263a80ee6f30ec55d25d75c5f9b7342ea99d9da
Tanner Lehett <john.cfplus@gmail.com>
2024-07-09 00:00:24 -0700

    Add more automated tests for the If component.

--- packages/func-comp/src/components/If/index.test.tsx
+++ packages/func-comp/src/components/If/index.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import If from './index';
+import { createRenderCycleTest } from '../../tests/perf-utils/render-cycle-testing';
 
 describe('If', () => {
     beforeEach(() => {
@@ -16,6 +17,104 @@ describe('If', () => {
             );
 
             expect(queryByText('If True')).toBeInTheDocument();
+        });
+    });
+
+    describe('when the If expression evaluates to false', () => {
+        const { queryByText } = render(
+            <If expr={false}>
+                <div>If False</div>
+            </If>,
+        );
+
+        expect(queryByText('If False')).not.toBeInTheDocument();
+    });
+
+    describe('when nesting If components', () => {
+        it('should render render the children of If components w/ `expr` evaluating to `true', () => {
+            const { queryByText } = render(
+                <>
+                    <If expr={true}>
+                        <If expr={true}>TT</If>
+                        <If expr={false}>TF</If>
+                    </If>
+                    <If expr={false}>
+                        <If expr={true}>FT</If>
+                        <If expr={false}>FF</If>
+                    </If>
+                </>,
+            );
+
+            expect(queryByText('TT')).toBeInTheDocument();
+            expect(queryByText('TF')).not.toBeInTheDocument();
+            expect(queryByText('FT')).not.toBeInTheDocument();
+            expect(queryByText('FF')).not.toBeInTheDocument();
+        });
+    });
+
+    describe('when used in a component that re-renders', () => {
+        it('should re-render without unmounting / remounting when the container re-renders', () => {
+            const {
+                renderTracker,
+                mountTracker,
+                unmountTracker,
+                updateContainer,
+            } = createRenderCycleTest((RenderTracker) => () => (
+                <If expr={true}>
+                    <RenderTracker />
+                </If>
+            ));
+
+            expect(renderTracker).toHaveBeenCalledTimes(1);
+            expect(mountTracker).toHaveBeenCalledTimes(1);
+            expect(unmountTracker).not.toHaveBeenCalled();
+
+            updateContainer();
+
+            expect(renderTracker).toHaveBeenCalledTimes(2);
+            expect(mountTracker).toHaveBeenCalledTimes(1);
+            expect(unmountTracker).not.toHaveBeenCalled();
+        });
+
+        it('should unmount / remount the If children when the `expr` prop changes', () => {
+            const {
+                renderTracker,
+                mountTracker,
+                unmountTracker,
+                updateContainer,
+            } = createRenderCycleTest(
+                (RenderTracker) =>
+                    ({ containerValue }) => (
+                        <If expr={Boolean(containerValue)}>
+                            <RenderTracker />
+                        </If>
+                    ),
+                1,
+                (prev) => {
+                    switch (prev) {
+                        case 1:
+                            return 2;
+                        default:
+                            return 0;
+                    }
+                },
+            );
+
+            expect(renderTracker).toHaveBeenCalledTimes(1);
+            expect(mountTracker).toHaveBeenCalledTimes(1);
+            expect(unmountTracker).not.toHaveBeenCalled();
+
+            updateContainer();
+
+            expect(renderTracker).toHaveBeenCalledTimes(2);
+            expect(mountTracker).toHaveBeenCalledTimes(1);
+            expect(unmountTracker).toHaveBeenCalledTimes(0);
+
+            updateContainer();
+
+            expect(renderTracker).toHaveBeenCalledTimes(2);
+            expect(mountTracker).toHaveBeenCalledTimes(1);
+            expect(unmountTracker).toHaveBeenCalledTimes(1);
         });
     });
 });
